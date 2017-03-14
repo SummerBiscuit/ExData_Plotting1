5e6c9f0ff04a80f8f17902be449edcffc29c7c2c
SummerBiscuit <chientai2009@gmail.com>
2017-03-14 00:55:39 -0700

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,11 +1,14 @@
 #plot 3
+#Read data
 data <- read.table("household_power_consumption.txt", sep=";",header=TRUE, stringsAsFactors = F)
 data$Date <-as.Date(data$Date, format = "%d/%m/%Y")
-dataNew<- subset(data, subset=(Date>="2007-02-01" & Date <= "2007-02-02"))
 
+#Specify timeframe
+dataNew<- subset(data, subset=(Date>="2007-02-01" & Date <= "2007-02-02"))
 
 dataNew <- transform(dataNew, newTime=as.POSIXct(paste(Date,Time)),"%d/%m/%Y %H:%M:%S")
 
+#plot
 png("plot3.png", width=480, height=480)
 plot(dataNew$newTime, as.numeric(dataNew$Sub_metering_1), type = "l", xlab = "", ylab ="Energy Sub Metering", col = "black")
 lines(dataNew$newTime,as.numeric(dataNew$Sub_metering_2),col="red")
