8694586d3a936977e4510fe05719cee1986c31a5
SummerBiscuit <chientai2009@gmail.com>
2017-03-14 00:56:16 -0700

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,11 +1,12 @@
 #Plot 4
 
-
+#Read data and specify time frame
 data <- read.table("household_power_consumption.txt", sep=";",header=TRUE, stringsAsFactors = F)
 data$Date <-as.Date(data$Date, format = "%d/%m/%Y")
 dataNew<- subset(data, subset=(Date>="2007-02-01" & Date <= "2007-02-02"))
 dataNew <- transform(dataNew, newTime=as.POSIXct(paste(Date,Time)),"%d/%m/%Y %H:%M:%S")
 
+#start plotting
 png("plot4.png", width=480, height=480)
 par(mfrow=c(2,2))
 #top left
